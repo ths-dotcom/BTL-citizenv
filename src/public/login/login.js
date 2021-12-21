ecdf069f8649b29671372891ba0034e0842a71d1
Nhat Anh <54491115+ZNhatAnhZ@users.noreply.github.com>
2021-12-21 13:06:29 +0700

    add class

--- src/public/login/login.js
+++ src/public/login/login.js
@@ -1,24 +1,33 @@
 // const { default: axios } = require("axios");
-
-$(document).ready(() => {
-    $('.submit-login-btn').on('click', () => {
-        if ($('#username').val() != '' && $('#password').val() != '') {
-            axios({
-                method: 'POST',
-                url: '/api/login',
-                data: {
-                    data: {
-                        username: $('#username').val(),
-                        password: $('#password').val()
-                    }
-                }
-            }).then((res) => {
-                console.log(res);
-                if (res.data.success) {
-                    document.cookie = `token=${res.data.token}`;
-                    window.location = '/home';
-                }
-            })
-        }
-    })
+requirejs.config({
+    paths: {
+        jquery: "https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js",
+        axios: "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"
+    }
 });
+
+define(["jquery", "axios"], function () {
+    $(document).ready(() => {
+        $('.submit-login-btn').on('click', () => {
+            if ($('#username').val() != '' && $('#password').val() != '') {
+                axios({
+                    method: 'POST',
+                    url: '/api/login',
+                    data: {
+                        data: {
+                            username: $('#username').val(),
+                            password: $('#password').val()
+                        }
+                    }
+                }).then((res) => {
+                    console.log(res);
+                    if (res.data.success) {
+                        document.cookie = `token=${res.data.token}`;
+                        window.location = '/home';
+                    }
+                })
+            }
+        })
+    });
+});
+
