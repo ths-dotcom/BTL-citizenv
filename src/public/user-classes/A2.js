e9395d938051418637398a6a448b4f52269b72d0
Admin <Dan123456dz@gmail.com>
2021-12-24 20:44:01 +0700

    a2

--- src/public/user-classes/A2.js
+++ src/public/user-classes/A2.js
@@ -128,6 +128,81 @@ define(['user-classes/Manager', 'jquery', 'axios'], function (Manager, $, axios)
 
         citizenInfoButtonClickEvent() {
             super.citizenInfoButtonClickEvent();
+            function resetAddressInput() {
+                $('#body-address-city').empty();
+                $('#body-address-city').append('<option selected disabled>Chon tinh thanh</option>');
+                $('#body-address-distric').empty();
+                $('#body-address-distric').append('<option selected disabled>Chọn huyện</option>');
+                $('#body-address-commune').empty();
+                $('#body-address-commune').append('<option selected disabled>Chọn xã</option>');
+                $('#body-address-hamlet').empty();
+                $('#body-address-hamlet').append('<option selected disabled>Chọn Thôn</option>');
+            };
+            axios({ // find all districts
+                method: 'GET',
+                url: '/api/district/list'
+            }).then((res) => {
+                if (res.data.success) {
+                    // reset all the input
+                    $('#body-address-city').empty();
+                    $('#body-address-city').append(`'<option selected disabled>${this.name}</option>'`);
+                    $('#body-address-distric').empty();
+                    $('#body-address-distric').append('<option selected disabled>Chọn huyện</option>');
+                    $('#body-address-commune').empty();
+                    $('#body-address-commune').append('<option selected disabled>Chọn xã</option>');
+                    $('#body-address-hamlet').empty();
+                    $('#body-address-hamlet').append('<option selected disabled>Chọn Thôn</option>');
+
+                    res.data.districts.forEach((e) => { // add district to the district input
+                        $('#body-address-distric').append(`<option value="${e.district_id}">${e.district_name}</option>`);
+                    })
+                } else {
+                    console.log(res);
+                }
+            });
+
+            $('#body-address-distric').on('change', () => {
+                axios({ // find all wards from a district
+                    method: 'GET',
+                    url: '/api/ward/list'
+                }).then((res) => {
+                    if (res.data.success) {
+                        // reset the remain input
+                        $('#body-address-commune').empty();
+                        $('#body-address-commune').append('<option selected disabled>Chọn xã</option>');
+                        $('#body-address-hamlet').empty();
+                        $('#body-address-hamlet').append('<option selected disabled>Chọn Thôn</option>');
+
+                        res.data.wards.forEach((e) => { // add communes to the commune input
+                            if (e.district_id == $('#body-address-distric').val()) {
+                                $('#body-address-commune').append(`<option value="${e.ward_id}">${e.ward_name}</option>`);
+                            };
+                        })
+                    } else {
+                        console.log(res);
+                    }
+                });
+            });
+            $('#body-address-commune').on('change', () => {
+                axios({ // find all hamlets from a ward
+                    method: 'GET',
+                    url: '/api/hamlet/list'
+                }).then((res) => {
+                    if (res.data.success) {
+                        // reset the remain input
+                        $('#body-address-hamlet').empty();
+                        $('#body-address-hamlet').append('<option selected disabled>Chọn Thôn</option>');
+
+                        res.data.wards.forEach((e) => { // add hamlets to hamlet input
+                            if (e.ward_id == $('#body-address-commune').val()) {
+                                $('#body-address-hamlet').append(`<option value="${e.hamlet_id}">${e.hamlet_name}</option>`);
+                            };
+                        })
+                    } else {
+                        console.log(res);
+                    }
+                });
+            });
         };
 
         monitoringProgressButtonClickEvent() {
